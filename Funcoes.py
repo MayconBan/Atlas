8b1bbdd775548a1c67f09e172ed2a00d91db6e63
MayconBan <mayconguilherme040@gmail.com>
2024-10-15 13:39:28 -0300

    Funcoes.py

--- Funcoes.py
+++ Funcoes.py
@@ -1,1 +1,68 @@
+n = "Atlas:"
+class desligar(object):
+   def __init__(self):
+   	self.msg = print("Atlas: Tchau")
+   	self.off =exit()
+   	def desligar(self):
+   		print()
+   		
+   		
+class taboada(object):
+	def __init__(self):
+         taboada =int(input('{} digite um numero - '.format(n)))
+         a = (taboada * 1)
+         b = (taboada * 2)
+         c = (taboada * 3)
+         d = (taboada * 4)
+         e = (taboada * 5)
+         f = (taboada * 6)
+         g = (taboada * 7)
+         h = (taboada * 8)
+         i = (taboada * 9)
+         j = (taboada * 10)
+         print('=-=-'*20)
+         print('{}×1={}'.format(taboada,a))
+         print('{}×2={}'.format(taboada,b))
+         print('{}×3={}'.format(taboada,c))
+         print('{}×4={}'.format(taboada,d))
+         print('{}×5={}'.format(taboada,e))
+         print('{}×6={}'.format(taboada,f))
+         print('{}×7={}'.format(taboada,g))
+         print('{}×8={}'.format(taboada,h))
+         print('{}×9={}'.format(taboada,i))
+         print('{}×10={}'.format(taboada,j))
+         print('=-=-'*20)
+         def taboada(self):
+         	print()
+
+class antessesor(object):
+   def __init__(self):
+   	n =int(input('{}digite um numero -').format(n))
+   	a = n - 1
+   	s = n + 1
+   	print('o numero digitado foi {}, seu anterssesor e {}, e seu sucessor e {}'.format(n,a,s))
+   	def antessesor(self):
+   		print()
+   		
+class calculadora(object):
+	def __init__(self):
+		
+		expressao_matematica = input('{}digite uma expresão matematica-'.format(n))
+		
+		resposta = eval(expressao_matematica)
+		
+		print('o resutado e ',resposta)
+		
+		def calculadora(self):
+			print()
+			
+class adivinha(object):
+	def __init__(self):
+		
+		h = input('{}pense num numero e digite-o '.format(n))
+		
+		print('{}voce pensou no numero {} '.format(n,h))
+		
+		def adivinha(self):
+			print()
 
